c4615fc320b0a1b6320317fc00c11adfca4bf919
Wanzhen Ng <ngwanzhen@gmail.com>
2017-06-24 15:20:57 +0800

    fixed letterCount object

--- js/letterCount.js
+++ js/letterCount.js
@@ -1,35 +1,14 @@
 
-function letterCount(word){
-  word=word.replace(/[!,&@.]|_/g, "").replace(/\s/g, "").toLowerCase()
-  // console.log(word)
-  var arr=word.split("")
-  var tempArr=word.split("")
+  function letterCount (word) {
+    word = word.replace(/[!,&@.]|_/g, '').replace(/\s/g, '').toLowerCase()
+    var arr = word.split('')
+    var ansObj = {}
 
- var counter=0
- var ansObj={
-   alpha: [],
-   counter: []}
-
- function count(letter){
-  for (var i=0; i<arr.length; i++){
-    if (tempArr.indexOf(letter) !== -1){
-      tempArr.splice(tempArr.indexOf(letter),1)
-      counter++
+    for (var i = 0; i < arr.length; i++) {
+      var letter = arr[i]
+      if (ansObj[letter]) { ansObj[letter] += 1 } else { ansObj[letter] = 1 }
     }
-   else if (tempArr.indexOf(letter) == -1){ }
+    return ansObj
   }
-  ansObj.alpha.push(letter)
-  ansObj.counter.push(counter)
-  // console.log(ansarr)
-  counter=0
-}
 
-arr.map(count)
-//console.log(ansObj)
-
-for (var j=0;j<arr.length;j++)
-if (ansObj.counter[j]!==0) {
-console.log (ansObj.alpha[j] + "-" + ansObj.counter[j])}
-}
-
-console.log(letterCount("A&p,p l.e!"))
+  console.log(letterCount('aPpl!e'))
